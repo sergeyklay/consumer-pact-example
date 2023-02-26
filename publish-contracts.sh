ed8a664e775789ba5640c8094d10926fc7224077
Serghei Iakovlev <egrep@protonmail.ch>
2023-02-26 12:41:54 +0100

    Provide ability to customize network name for broker

--- publish-contracts.sh
+++ publish-contracts.sh
@@ -28,7 +28,7 @@ $containerd_cli run -ti \
   -e PACT_BROKER_BASE_URL="${PACT_BROKER_BASE_URL:-http://broker_app:9292}" \
   -e PACT_BROKER_USERNAME="${PACT_BROKER_USERNAME:-pactbroker}" \
   -e PACT_BROKER_PASSWORD="${PACT_BROKER_USERNAME:-pactbroker}" \
-  --network broker_backend \
+  --network "${BROKER_NETWORK:-broker_default}" \
   pactfoundation/pact-cli:latest \
   publish /pacts \
     --consumer-app-version="${CONSUMER_VERSION:-$(python setup.py --version)}" \
