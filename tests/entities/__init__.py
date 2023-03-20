d769709300acfea01853ba733669232239b338fa
Serghei Iakovlev <egrep@protonmail.ch>
2023-03-20 01:28:18 +0100

    test: cover base entity

---
+++ tests/entities/__init__.py
@@ -0,0 +1,8 @@
+# This file is part of the Consumer API example.
+#
+# Copyright (C) 2023 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# For the full copyright and license information, please view
+# the LICENSE file that was distributed with this source code.
+
+"""The top-level module for the consumer entities testing."""
