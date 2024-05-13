f6f2841d3051ac5f550807bdf715715420683731
Serghei Iakovlev <egrep@protonmail.ch>
2024-05-13 14:55:37 +0200

    feat(test): remove Format utils needed only for pact-python from v1.x

--- tests/factories.py
+++ tests/factories.py
@@ -23,8 +23,6 @@ class Format(BaseFormat):
 
         self.etag = self.etag()
         self.last_modified = self.last_modified()
-        self.iso_datetime = self.iso_8601_datetime()
-        self.iso_datetime_ms = self.iso_8601_datetime(with_ms=True)
         self.media_type_json = self.media_type_json()
 
     def url(self, path: str, generate: str):
@@ -65,52 +63,6 @@ class Format(BaseFormat):
             f'''"{hashlib.sha1(bytes('{}', 'utf-8')).hexdigest()}"'''
         )
 
-    def iso_8601_datetime(self, with_ms=False):
-        """
-        Match a string for a full ISO 8601 Date.
-
-        Does not do any sort of date validation, only checks if the string is
-        according to the ISO 8601 spec.
-
-        This method differs from :func:`~pact.Format.timestamp`,
-        :func:`~pact.Format.date` and :func:`~pact.Format.time` implementations
-        in that it is more stringent and tests the string for exact match to
-        the ISO 8601 dates format.
-
-        Without `with_ms` will match string containing ISO 8601 formatted dates
-        as stated bellow:
-
-        * 2016-12-15T20:16:01
-        * 2010-05-01T01:14:31.876
-        * 2016-05-24T15:54:14.00000Z
-        * 1994-11-05T08:15:30-05:00
-        * 2002-01-31T23:00:00.1234-02:00
-        * 1991-02-20T06:35:26.079043+00:00
-
-        Otherwise, ONLY dates with milliseconds will match the pattern:
-
-        * 2010-05-01T01:14:31.876
-        * 2016-05-24T15:54:14.00000Z
-        * 2002-01-31T23:00:00.1234-02:00
-        * 1991-02-20T06:35:26.079043+00:00
-
-        :param with_ms: Enforcing millisecond precision.
-        :type with_ms: bool
-        :return: a Term object with a date regex.
-        :rtype: Term
-        """
-        date = [1991, 2, 20, 6, 35, 26]
-        if with_ms:
-            matcher = self.RegexesEx.iso_8601_datetime_ms.value
-            date.append(79043)
-        else:
-            matcher = self.RegexesEx.iso_8601_datetime.value
-
-        return Term(
-            matcher,
-            datetime.datetime(*date, tzinfo=datetime.timezone.utc).isoformat()
-        )
-
     def media_type_json(self):
         """Match json media type used in the Content-Type header.
 
@@ -127,10 +79,6 @@ class Format(BaseFormat):
     class RegexesEx(Enum):
         """Regex Enum for common formats."""
 
-        iso_8601_datetime = r'^\d{4}-[01]\d-[0-3]\d\x54[0-2]\d:[0-6]\d:' \
-                            r'[0-6]\d(?:\.\d+)?(?:(?:[+-]\d\d:\d\d)|\x5A)?$'
-        iso_8601_datetime_ms = r'^\d{4}-[01]\d-[0-3]\d\x54[0-2]\d:[0-6]\d:' \
-                               r'[0-6]\d\.\d+(?:(?:[+-]\d\d:\d\d)|\x5A)?$'
         last_modified = r'^[A-Za-z]{3},\s\d{2}\s[A-Za-z]{3}\s\d{4}\s\d{2}:' \
                         r'\d{2}:\d{2}\sGMT$'
         etag = r'^(?:\x57\x2f)?"(?:[\x21\x23-\x7e]*|\r\n[\t ]|\.)*"$'
@@ -164,5 +112,5 @@ class ProductFactory(factory.DictFactory):
     discount = Format().decimal
     rating = Format().decimal
     stock = Format().integer
-    created_at = Format().iso_datetime_ms
-    updated_at = Format().iso_datetime_ms
+    created_at = BaseFormat().iso_datetime_ms
+    updated_at = BaseFormat().iso_datetime_ms
